6784e55939e98978e950ea2fb125e39d59d79841
Hugo Valent <valenthugo@hotmail.com>
2022-10-08 16:23:28 +0100

    add customer and bill models

--- billsapp/models.py
+++ billsapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phonenumber = models.CharField(max_length=255)
+
+
+class Bill(models.Model):
+
+    class Bill_products(models.TextChoices):
+        COOLPRODUCT = 'CP', _('Cool Product')
+        YAYPRODUCT = 'YP', _('Yay Product')
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.CharField(max_length=2,choices=Bill_products.choices,)
+    price = models.IntegerField(min=0)
+    discount = models.IntegerField(min=0,max=100, default=0)
